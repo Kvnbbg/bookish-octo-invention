59db795b13458356d9214cae70610dce1d98e53e
Kvnbbg <kvnbbg@gmail.com>
2023-12-14 00:35:52 +0100

    refractoring

--- app.py
+++ app.py
@@ -1,37 +1,51 @@
 import os
 from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# Sample recipe data
-recipes = [
-    {
-        'title': 'Pasta Carbonara',
-        'image': 'https://example.com/pasta-carbonara.jpg',
-        'ingredients': ['Spaghetti', 'Eggs', 'Bacon', 'Parmesan Cheese'],
-        'instructions': 'Cook spaghetti. Fry bacon. Mix eggs and cheese. Combine all.'
-    },
-    {
-        'title': 'Chocolate Cake',
-        'image': 'https://example.com/chocolate-cake.jpg',
-        'ingredients': ['Flour', 'Sugar', 'Cocoa Powder', 'Eggs', 'Milk'],
-        'instructions': 'Mix dry ingredients. Add wet ingredients. Bake at 350°F.'
-    }
-]
+# Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Initialize the database
+db = SQLAlchemy(app)
+
+# Define the Recipe model
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    # Add more fields as needed
+
+    def __repr__(self):
+        return f'<Recipe {self.id}: {self.title}>'
+
+# Create tables (run this only once)
+db.create_all()
+
+# Check if templates exist
+required_templates = ['index.html', 'recipe.html', '404.html', '500.html']
+missing_templates = [template for template in required_templates if not os.path.exists(f'templates/{template}')]
+if missing_templates:
+    print(f"One or more templates are missing: {', '.join(missing_templates)}")
+    exit(1)
+
+# Routes
 @app.route('/')
 def index():
+    recipes = Recipe.query.all()
     return render_template('index.html', recipes=recipes)
 
 @app.route('/recipe/<int:recipe_id>')
 def show_recipe(recipe_id):
-    if recipe_id < 0 or recipe_id >= len(recipes):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
         app.logger.error(f'Recipe with id {recipe_id} not found')
         abort(404)  # Recipe not found
 
-    recipe = recipes[recipe_id]
     return render_template('recipe.html', recipe=recipe)
 
+# Error handlers
 @app.errorhandler(404)
 def not_found_error(error):
     app.logger.error('Page not found: %s', (request.path))
@@ -48,10 +62,4 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    # Check if templates exist
-    missing_templates = [template for template in ['index.html', 'recipe.html', '404.html', '404.html', '500.html'] if not os.path.exists(f'templates/{template}')]
-    if missing_templates:
-        print(f"One or more templates are missing: {', '.join(missing_templates)}")
-        exit(1)
-
     app.run(debug=True)
