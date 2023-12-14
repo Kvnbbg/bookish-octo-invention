96c7f54822c4b283c3b73f116d6031abae2e635d
Kvnbbg <kvnbbg@gmail.com>
2023-12-14 22:49:09 +0100

    fix

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
 import os
 import sqlite3
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
+
+from recipe import initialize_database
 
 app = Flask(__name__)
 
-# Connect to the SQLite database
-conn = sqlite3.connect('recipes.db')
-cursor = conn.cursor()
+# Initialize the database by running the code from recipe.py
+initialize_database()
 
 @app.route('/')
 def index():
+    # Fetch all recipes from the database
+    cursor.execute('SELECT * FROM recipes')
+    recipes = cursor.fetchall()
     return render_template('index.html', recipes=recipes)
 
 @app.route('/recipe/<int:recipe_id>')
 def show_recipe(recipe_id):
-    if recipe_id < 0 or recipe_id >= len(recipes):
+    # Fetch a specific recipe by its ID from the database
+    cursor.execute('SELECT * FROM recipes WHERE id = ?', (recipe_id,))
+    recipe = cursor.fetchone()
+
+    if not recipe:
         app.logger.error(f'Recipe with id {recipe_id} not found')
         abort(404)  # Recipe not found
 
-    recipe = recipes[recipe_id]
     return render_template('recipe.html', recipe=recipe)
 
 @app.errorhandler(404)
 def not_found_error(error):
-    app.logger.error('Page not found: %s', (request.path))
+    app.logger.error('Page not found: %s', request.path)
     return render_template('404.html'), 404
 
 @app.errorhandler(400)
 def bad_request_error(error):
-    app.logger.error('Bad Request: %s', (error))
+    app.logger.error('Bad Request: %s', error)
     return render_template('400.html'), 400
 
 @app.errorhandler(500)
 def internal_error(error):
-    app.logger.error('Server Error: %s', (error))
+    app.logger.error('Server Error: %s', error)
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
     # Check if templates exist
-    missing_templates = [template for template in ['index.html', 'recipe.html', '404.html', '404.html', '500.html'] if not os.path.exists(f'templates/{template}')]
+    missing_templates = [template for template in ['index.html', 'recipe.html', '404.html', '500.html'] if not os.path.exists(f'templates/{template}')]
     if missing_templates:
         print(f"One or more templates are missing: {', '.join(missing_templates)}")
         exit(1)
---
+++ recipe.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+def initialize_database():
+    # Connect to the SQLite database or create it if it doesn't exist
+    conn = sqlite3.connect('recipes.db')
+    cursor = conn.cursor()
+
+    # Create a 'recipes' table in the database if it doesn't exist
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS recipes (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            image TEXT NOT NULL,
+            ingredients TEXT NOT NULL,
+            instructions TEXT NOT NULL
+        )
+    """)
+
+    # Sample data - Inserting some initial recipes into the table if it's empty
+    cursor.execute("SELECT COUNT(*) FROM recipes")
+    if cursor.fetchone()[0] == 0:
+        sample_recipes = [
+            {
+                'title': 'Pasta Carbonara',
+                'image': 'https://example.com/pasta-carbonara.jpg',
+                'ingredients': 'Spaghetti, Eggs, Bacon, Parmesan Cheese',
+                'instructions': 'Cook spaghetti. Fry bacon. Mix eggs and cheese. Combine all.'
+            },
+            {
+                'title': 'Chocolate Cake',
+                'image': 'https://example.com/chocolate-cake.jpg',
+                'ingredients': 'Flour, Sugar, Cocoa Powder, Eggs, Milk',
+                'instructions': 'Mix dry ingredients. Add wet ingredients. Bake at 350°F.'
+            }
+        ]
+
+        for recipe in sample_recipes:
+            cursor.execute('''
+                INSERT INTO recipes (title, image, ingredients, instructions)
+                VALUES (?, ?, ?, ?)
+            ''', (recipe['title'], recipe['image'], recipe['ingredients'], recipe['instructions']))
+        conn.commit()
+
+    conn.close()
