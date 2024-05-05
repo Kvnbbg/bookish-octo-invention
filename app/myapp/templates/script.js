7c251b7ae49adb8fe77e8906eafd34e193b3887d
Kevin Marville <kevinmarville@gmail.com>
2024-05-05 23:05:14 +0200

    Add script.js and index_py.html templates

---
+++ app/myapp/templates/script.js
@@ -0,0 +1,9 @@
+const addButton = document.getElementById("addButton");
+
+addButton.addEventListener("click", addGoal);
+
+function addGoal() {
+  // This function will be implemented to handle adding new items
+  // You can prompt the user for details and create new elements in the items section
+  alert("This functionality is not yet implemented. Click OK to dismiss.");
+}
