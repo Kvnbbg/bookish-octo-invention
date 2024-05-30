32d909b7dc6487affcedc3e8b3547cacb2703e5d
Kevin Marville <kevinmarville@gmail.com>
2024-05-30 00:19:19 +0000

    chore: Remove unused files and dependencies

--- app/myapp/extensions.py
+++
@@ -1,5 +0,0 @@
-# myapp/extensions.py
-from flask_login import LoginManager
-
-login_manager = LoginManager()
-login_manager.login_view = "views.login"
--- app/myapp/instance/config.py
+++
@@ -1,9 +0,0 @@
-# bookish-octo-invention/instance/config.py
-# instance/config.py
-
-# This file contains instance-specific configuration overrides.
-
-# Override database settings for this instance SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://your_database_user:your_password@localhost/your_database_name'
-
-# Toggle debug mode for development or production DEBUG = False  # Set to True for development, False for production
-
