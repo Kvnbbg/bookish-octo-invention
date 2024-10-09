68e87c393e3a3604bccfa84070438fd9802634e0
kvnbbg <kvnbbg@gmail.com>
2024-10-09 11:20:46 +0000

    yyy

--- run.js
+++ run.js
@@ -1,18 +1,29 @@
-// run.js
 import { spawn } from 'child_process';
 
-const serverProcess = spawn('node', ['./app/server.js']); 
+function startServer() {
+  const serverProcess = spawn('node', ['./app/server.js']);
 
-serverProcess.stdout.on('data', (data) => {
-  console.log(`Server: ${data}`);
-});
+  serverProcess.stdout.on('data', (data) => {
+    console.log(`Server Output: ${data}`);
+  });
 
-serverProcess.stderr.on('data', (data) => {
+  serverProcess.stderr.on('data', (data) => {
     console.error(`Server Error: ${data}`);
   });
-  
-  serverProcess.on('close', (code) => {
-    console.log(`Server  
-   process exited with code ${code}`);  
-  
+
+  serverProcess.on('error', (error) => {
+    console.error(`Failed to start the server process: ${error.message}`);
   });
+
+  serverProcess.on('close', (code) => {
+    console.log(`Server process exited with code ${code}`);
+    // Automatically restart the server if it exits with an error code
+    if (code !== 0) {
+      console.log('Restarting server...');
+      startServer();
+    }
+  });
+}
+
+// Start the server
+startServer();
